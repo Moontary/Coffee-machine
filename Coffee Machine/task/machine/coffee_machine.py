30ee8f4853625f15dad2d245a6de957040341376
Moontary <kostiantyntkachenko@gmail.com>
2020-11-19 11:25:10 +0200

    Second stage
    Second stage of the project

---
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -0,0 +1,6 @@
+# Write your code here
+cups = int(input("Write how many cups of coffee you will need: "))
+print(f'''For {cups} cups of coffee you will need:
+{200 * cups} ml of water
+{50 * cups} ml of milk
+{15 * cups} g of coffee beans''')
